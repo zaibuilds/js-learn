8d6bd60908ac711cac24fdada8818ae38ed3bed2
Zai Builds <hello@zaibuilds.io>
2022-12-13 02:47:21 +0000

    more code lol

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ let ourName = "Zai Builds" // let can only be used within the scope of where you
 const pi = 3.14 // const is a variable that can never change //
 
 // Storing Values with the Assignment Operator //
+
+var a; // Declaring Variable
+var b = 2; // Declaring + Assigning Variable
+
+a = 7; // Assigned 7 to a
+
+b = a // Assigned contents of variable a to varable b 
