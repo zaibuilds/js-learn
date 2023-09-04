4632f22f83bac2de3fcab591d9de6d20f8261611
Zai Builds <hello@zaibuilds.io>
2023-09-04 09:58:09 +0100

    concat method and template literals

--- mycurriculum/basicvariablemanipulation.js
+++ mycurriculum/basicvariablemanipulation.js
@@ -2,6 +2,23 @@ let myName = "Zai";
 
 let myAge = 22;
 
-console.log(myName);
+// console.log(myName);
 
-console.log(myAge);
+// console.log(myAge);
+
+let numOne = 5;
+let numTwo = 6;
+
+let mySum = numOne + numTwo;
+
+// console.log(mySum);
+// console.log(mySum);
+
+let wordOne = "Cat";
+let wordTwo = "Claws";
+
+let myString = wordOne.concat(" ", wordTwo); // Concat method
+
+console.log(myString);
+
+console.log(`Mononoke is a ${wordOne}`); // Template literals 
