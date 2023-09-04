b6e9ad80e4a6be48089fd861d5fe838d85c70bcc
Zai Builds <hello@zaibuilds.io>
2023-09-04 17:14:30 +0100

    playing with if statements

--- mycurriculum/basicvariablemanipulation.js
+++ mycurriculum/basicvariablemanipulation.js
@@ -1,3 +1,5 @@
+const { string } = require("nunjucks/src/filters");
+
 let myName = "Zai";
 
 let myAge = 22;
@@ -16,12 +18,13 @@ let mySum = numOne + numTwo;
 
 let wordOne = "Cat";
 let wordTwo = "Claws";
+let wordThree = "cutiepie";
 
 let myString = wordOne.concat(" ", wordTwo); // Concat method
 
 console.log(myString);
 
-console.log(`Mononoke is a ${wordOne}`); // Template literals 
+console.log(`Mononoke is a ${wordThree}`); // Template literals 
 
 let isStudent = true;
 
@@ -40,3 +43,51 @@ const person = {
     gender: "female",
     age: "22"
 }
+
+// console.log(person.name);
+console.log(person.age);
+
+person.name = "Akeem"
+
+console.log(person.name);
+
+const pi = 3.1459;
+
+let radius = 6;
+
+let circleArea = pi * radius;
+
+console.log(circleArea);
+
+const myFruits = ["lemon", "orange", "banana", "apple"];
+
+myFruits.push("grapefruit"); // Push method adds an object to an array
+
+console.log(myFruits);
+
+myFruits.pop(); // Pop method removes the last object from an array
+
+console.log(myFruits);
+
+let myFriend = "Wez";
+
+if ( typeof myFriend === "string" ) {
+    console.log("It's a string!")
+};
+
+let mySexuality = undefined;
+
+if (typeof mySexuality === "undefined") {
+    console.log("I'm just finding myself...")
+}
+
+let myBae = 1;
+
+if (typeof myBae === "number") {
+    console.log("Lover Girl Season!")
+}
+
+console.log(typeof mySum);
+
+let mySentence = "Zai is a lover girl, but only sometimes";
+
