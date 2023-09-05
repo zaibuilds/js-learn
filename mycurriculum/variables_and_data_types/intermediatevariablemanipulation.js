fe56017d9431bd2ec44f1f3c13a8936321ec3781
Zai Builds <hello@zaibuilds.io>
2023-09-05 15:56:08 +0100

    working on intermediate variable manipulation

---
+++ mycurriculum/variables_and_data_types/intermediatevariablemanipulation.js
@@ -0,0 +1,45 @@
+// 1 Create a function that swaps the values of two variables.
+
+// Destructuring Assignment
+
+
+let a = 1;
+let b = 2;
+
+// Testing
+
+console.log(a);
+console.log(b);
+
+// 
+
+
+
+
+
+
+// Using a Temporary Variable 
+
+let varOne = "Variable 1";
+let varTwo = "Variable 2";
+ 
+// Testing 
+
+// console.log(varOne);
+// console.log(varTwo);
+
+// Create a temporary variable
+
+let temp;
+
+// Swap variables
+
+temp = varOne;
+varOne = varTwo;
+varTwo = temp;
+
+// Testing
+
+console.log(varOne);
+console.log(varTwo);
+
