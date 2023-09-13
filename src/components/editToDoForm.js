7749a660f056b5d1038d81d62b6482cf6a60a375
Zai Builds <hello@zaibuilds.io>
2023-09-14 00:07:21 +0100

    creating components

---
+++ src/components/editToDoForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const editToDoForm = () => {
+  return (
+    <div>editToDoForm</div>
+  )
+}
---
+++ src/components/todoWrapper.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const todoWrapper = () => {
+  return (
+    <div>todoWrapper</div>
+  )
+}
