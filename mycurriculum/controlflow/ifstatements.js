695e5f74c036692720850053371f21b6eb0e2bf7
Zai Builds <hello@zaibuilds.io>
2023-09-20 16:58:54 +0100

    learning control flow

--- mycurriculum/controlflow/ifstatements.js
+++ mycurriculum/controlflow/ifstatements.js
@@ -1,18 +1,48 @@
 // 1 - Write an if statement to check if a number is positive 
 
-let myNumber = 4;
+let myNumber = -4;
 
 if (myNumber > 0) {
     console.log("This number is positive");
+} else {
+    console.log("This number is negative")
 }
 
 // 2 - Write an if-else statement to determine if a user is old enough to vote (age >= 18)
 
-const age = 21;
+const age = 16;
 
 if (age >= 18) {
     console.log("This user is old enough to vote");
 } else {
     console.log("This user is too young to vote");
 }
+
+// 3 - Write an if-else statement to determine if a number is even or odd 
+
+let thisNumber = 21;
+
+if (thisNumber % 2 === 0) {
+    console.log("This number is even")
+} else {
+    console.log("This number is odd")
+};
+
+// 4 - Write a nested if statement to check if a number is positive and even
+
+let specialNumber = 97; 
+
+if (specialNumber > 0) {
+    if (specialNumber % 2 === 0) {
+        console.log("This number is positive and even")
+    } else {
+        console.log("This number is positive and odd")
+    }
+} else {
+    if (specialNumber % 2 === 0) {
+        console.log("This number is negative and even")
+    } else {
+        console.log("This number is negative and odd")
+    }
+};
 
