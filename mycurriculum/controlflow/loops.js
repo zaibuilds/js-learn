3b19ff0a4cdd89db2ae7d89886da43da34082fcd
Zai Builds <hello@zaibuilds.io>
2023-09-20 20:07:24 +0100

    playing with loops

--- mycurriculum/controlflow/loops.js
+++ mycurriculum/controlflow/loops.js
@@ -3,3 +3,18 @@
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }
+
+// 2 - Write a for loop to calculate the sum of all even numbers from 1 to 100
+
+let totalSum = 0;
+
+for (let num = 2; num <= 100; num++) {
+    // Check if the current number is even 
+
+    if (num % 2 === 0) {
+        totalSum += num;
+    }
+
+}
+
+console.log("The total sum of even numbers from 1 to 100 equals", totalSum);
