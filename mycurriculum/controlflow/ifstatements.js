15be22d05780beff4e6f5a8058d287fa1c0e25af
Zai Builds <hello@zaibuilds.io>
2023-09-20 19:42:04 +0100

    study

--- mycurriculum/controlflow/ifstatements.js
+++ mycurriculum/controlflow/ifstatements.js
@@ -98,7 +98,8 @@ if (year % 4 === 0 && year % 100 === 0) {
 let thisString= "";
 
 // Method 1 - Trim method 
-// The trim method removes white space from
+/* The trim method removes white space from both the beginning and end of a string. 
+After trimming, if the string has a length of zero, it is considered empty. */
 
 if (thisString.trim() === "") {
     console.log("This string is empty")
@@ -107,6 +108,7 @@ if (thisString.trim() === "") {
 };
 
 // Method 2 - Using .length method
+// Check if the length of the string is equal to 0 to determine if it's empty
 
 if (thisString.length === 0) {
     console.log("This string is empty")
@@ -114,4 +116,21 @@ if (thisString.length === 0) {
     console.log("This string is not empty")
 };
 
+// Method 3 - Using a conditional check
+// You can simply use a conditional statement to check if the string is empty or only contains whitespace characters
 
+if (!thisString) {
+    console.log("This string is empty or only contains whitespace")
+} else {
+    console.log("This string is not empty")
+};
+
+// 10 - Write an if-else statement to determine if a number is divisible by both 3 and 5
+
+let mySpecialNumber = 33;
+
+if (mySpecialNumber % 3 === 0 && mySpecialNumber % 5 === 0) {
+    console.log("This number is divisible by both 3 and 5");
+} else {
+    console.log("This number isn't divisible by both 3 and 5");
+};
