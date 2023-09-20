013590b1fdd42a30aac50afa566b0e473858f395
Zai Builds <hello@zaibuilds.io>
2023-09-20 16:48:49 +0100

    studying control flow

--- mycurriculum/controlflow/ifstatements.js
+++ mycurriculum/controlflow/ifstatements.js
@@ -6,3 +6,13 @@ if (myNumber > 0) {
     console.log("This number is positive");
 }
 
+// 2 - Write an if-else statement to determine if a user is old enough to vote (age >= 18)
+
+const age = 21;
+
+if (age >= 18) {
+    console.log("This user is old enough to vote");
+} else {
+    console.log("This user is too young to vote");
+}
+
