667596e665fd02c6dc15764f053f7b14b69d65e0
Zai Builds <hello@zaibuilds.io>
2023-09-20 21:44:53 +0100

    maths

---
+++ mycurriculum/operators/operators.js
@@ -0,0 +1,31 @@
+// 1 - Write an expression that adds two numbers
+
+let myExpression = 2 + 4;
+
+console.log(myExpression);
+
+// 2 - Subtract one number from another
+
+let mySubtraction = 8 - 4;
+
+console.log(mySubtraction); 
+
+// 3 - Multiply two numbers 
+
+let myMultiplication = 2 * 9;
+
+console.log(myMultiplication); 
+
+// 4 - Divide one number by another
+
+let myDivision = 8 / 4;
+
+console.log(myDivision);
+
+// 5 - Find the the remainder when dividing one number by another
+
+let myRemainder = 28 % 3;
+
+console.log(myRemainder);
+
+// 6 - Calculate the square root of a number
