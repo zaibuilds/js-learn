c424efacbfddd1571eb70b1fd6a4ff9515f34ea7
Zai Builds <hello@zaibuilds.io>
2023-09-23 21:32:53 +0100

    Loops

--- mycurriculum/controlflow/loops.js
+++ mycurriculum/controlflow/loops.js
@@ -47,3 +47,26 @@ function findFactorial(number) {
     const num = 4;
     const result = findFactorial(num);
     console.log(`The factorial of ${num} is: ${result}`);
+
+    // 4 - Write a do-while loop to ask the user for a password until they enter the correct one.
+
+    const myPassword = "PrettyLittleBird22";
+    let userInput;
+    let attempts = 0;
+
+    do {
+        userInput = prompt("Please enter your password");
+
+        if (userInput === myPassword) {
+            console.log("Your password is correct, continue your sign in");
+            break;  // Exit the loop when the correct password is inputted
+        } else {
+            attempts++;
+            console.log("Your password is incorrect, please try again");
+        }
+
+    } while (attempts < 3);
+
+     if (attempts === 3) {
+        console.log("Your account has been temporarily locked, please try again later.")
+     }
