a70a96894906e4d70b973e73fd9f999ae4fa8b90
Zai Builds <hello@zaibuilds.io>
2023-09-26 21:33:53 +0100

    learning

---
+++ mycurriculum/arrays/basicarrayoperations.js
@@ -0,0 +1,33 @@
+// 1 - Create an empty array
+
+const emptyArray = [];
+
+// 2 - Create an array with five elements
+
+const fiveElements = [1,2,3,4,5];
+
+// 3 - Access the first element of an array
+
+console.log(fiveElements[0]);
+
+// 4 - Access the last element of an array
+
+console.log(fiveElements[4]);
+
+// 5 - Add an element to the end of an array
+
+fiveElements.push(6);
+
+// Test
+
+console.log(fiveElements);
+
+// 6 - Remove the last element from an array
+
+fiveElements.pop();
+
+// Test
+
+console.log(fiveElements);
+
+// 7 - Add an element to the beginning of an array
