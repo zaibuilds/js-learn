30f2f2419fcaeb2b698cc9cad326983f300ddaa3
Zai Builds <hello@zaibuilds.io>
2023-09-27 21:50:34 +0100

    more programming

--- mycurriculum/controlflow/nestedcontrolflow.js
+++ mycurriculum/controlflow/nestedcontrolflow.js
@@ -24,3 +24,22 @@ function checkDiscount () {
 };
 
 checkDiscount();
+
+
+// 2 - Write a program that checks if a year is a leap year and, if it is, checks if it's divisible by 400.
+
+var year = 2450;
+
+function checkLeapYear () {
+    if (year % 4 === 0) {        
+        if (year % 400 === 0) {
+            console.log("This is a leap year and divisible by 400")
+        } else {
+            console.log("This is just a leap year")
+        }
+    } else {
+        console.log("This is not a leap year")
+    }
+};
+
+checkLeapYear();
