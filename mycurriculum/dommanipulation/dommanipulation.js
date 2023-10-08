1bcd9042da2c674e55f040f293705ee0498f5de1
Zai Builds <hello@zaibuilds.io>
2023-10-08 15:55:03 +0100

    studying

---
+++ mycurriculum/dommanipulation/dommanipulation.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const button = document.getElementById("myButton");
+    button.addEventListener("click", function() {
+        alert("Button clicked!");
+    });
+
+    const paragraph = document.getElementById("myParagraph");
+    
+
+});
