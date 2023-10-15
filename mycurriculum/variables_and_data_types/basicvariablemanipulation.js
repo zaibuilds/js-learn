a0295babfcbb991620bce3c7bbf7c397a353868a
Zai Builds <hello@zaibuilds.io>
2023-10-15 23:11:04 +0100

    studying

--- mycurriculum/variables_and_data_types/basicvariablemanipulation.js
+++ mycurriculum/variables_and_data_types/basicvariablemanipulation.js
@@ -17,6 +17,8 @@ console.log(mySum)
 
 // 4 - Concatenate two strings and store the result in a variable 
 
+// concat method
+
 let wordOne = "My";
 let wordTwo = "milkshake";
 
@@ -24,3 +26,22 @@ let mySentence = wordOne.concat(" ", wordTwo);
 
 console.log(mySentence);
 
+// 
+
+//
+
+//
+
+// 5 - Declare a variable isStudent and assign a boolean value to it
+
+let isStudent = true;
+
+// 6 - Create a variable myArray and assign an array of numbers to it
+
+const myArray = [2,4,5,7];
+
+// 7 - Access the third element of myArray
+
+console.log (myArray[2]);
+
+// 8 - Modify the second element of myArray
