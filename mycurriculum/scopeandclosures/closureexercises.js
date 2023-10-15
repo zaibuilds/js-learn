2052ce0609bd31098bae843b9605df5f96ed91ff
Zai Builds <hello@zaibuilds.io>
2023-10-15 18:53:40 +0100

    studying

--- mycurriculum/scopeandclosures/closureexercises.js
+++ mycurriculum/scopeandclosures/closureexercises.js
@@ -11,8 +11,22 @@ function myFunction () {
 }
 
 myFunction(); // Returns the result of the inner function
-myInnerFunction(); // Returns error myInnerFunction is not defined
+// myInnerFunction(); // Returns error myInnerFunction is not defined
 
 
 // 2 - Create a closure that captures a variable from the outer scope and prints its value.
 
+function outerFunction() {
+    let outerVariable = 10;
+
+    function innerFunction() {
+        console.log(outerVariable);
+    };
+
+    return innerFunction;
+}
+
+outerFunction(); 
+
+const closure = outerFunction(); // Closure is now a reference to innerFunction
+closure(); // This still logs 10 even though outerFunction has completed
