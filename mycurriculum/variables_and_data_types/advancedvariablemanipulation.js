12bf174d18e8cb72b386a371adad8b78d2a5bfbf
Zai Builds <hello@zaibuilds.io>
2023-10-20 16:26:09 +0100

    studying

--- mycurriculum/variables_and_data_types/advancedvariablemanipulation.js
+++ mycurriculum/variables_and_data_types/advancedvariablemanipulation.js
@@ -1,1 +1,32 @@
-// 1 - 
+// 1 - Create a closure that remembers a variable from an outer scope
+
+let myName = "Zai";
+let closure; // Declare closure in the global scope
+
+
+function greetUser() {
+    closure = myName; // Assigne myName to the closure variable
+    console.log(`Welcome ${closure}`);
+}
+
+greetUser();
+
+console.log(closure); // Now closure can be accessed in both the local scope and the global scope
+
+
+// 2 - Use the forEach method to iterate over an array and perform an action
+
+const myNumberArray = [2,4,6,8];
+
+myNumberArray.forEach(number => {
+   console.log(number * 2);
+});
+
+
+// Create a class for a simple object and instantiate it
+
+class cuteKitten {
+    constructor() {
+        
+    }
+}
