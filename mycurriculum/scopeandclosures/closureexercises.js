e42050f6efa9b2709166ca55cdab85c224961454
Zai Builds <hello@zaibuilds.io>
2023-11-04 20:23:32 +0000

    closures

--- mycurriculum/scopeandclosures/closureexercises.js
+++ mycurriculum/scopeandclosures/closureexercises.js
@@ -30,3 +30,35 @@ outerFunction();
 
 const closure = outerFunction(); // Closure is now a reference to innerFunction
 closure(); // This still logs 10 even though outerFunction has completed
+
+// 3 - Create a function that takes a parameter and returns a function that uses that parameter.
+
+function myMathFunction(num) {
+    return function myInnerMathFunction(){
+        console.log(`This number is ${num}`);
+    }
+}
+
+const innerFunction = myMathFunction(3);
+innerFunction();
+
+// 4 - Create a counter function using closures that increments a value each time it's called.
+
+
+// 5 - Create a function that keeps track of the total number of function calls.
+
+
+// 6 - Create a function that calculates the sum of two numbers and returns a closure to compute the sum.
+
+
+// 7 - Create a function that generates a random number between two specified values and returns it as a closure.
+
+
+// 8 - Create a function that memorises the results of expensive function calls using closures.
+
+
+// 9 - Create a function that logs the timestamp of each function call.
+
+
+// 10 - Create a function that checks if a given number is even or odd using a closure.
+
