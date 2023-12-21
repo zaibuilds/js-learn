c7e84907dd27521dd062bb97b09833cf867268c8
Zai Builds <hello@zaibuilds.io>
2023-12-21 22:00:51 +0000

    studying

--- mycurriculum/variables_and_data_types/intermediatevariablemanipulation.js
+++ mycurriculum/variables_and_data_types/intermediatevariablemanipulation.js
@@ -18,6 +18,12 @@ b = store;
 // Declare and initialise multiple variables in a single line.
 
 let d = 4;
+    e = 5;
+    f = 6;
+
+    // console.log(d)
+    // console.log(e)
+    // console.log(f)
     
 
 
