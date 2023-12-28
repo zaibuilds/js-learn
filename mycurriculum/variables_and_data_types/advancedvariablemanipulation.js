ed5c9bf93fe680039fec367952821d1eadeaaf65
Zai Builds <hello@zaibuilds.io>
2023-12-28 00:07:28 +0000

    studying

--- mycurriculum/variables_and_data_types/advancedvariablemanipulation.js
+++ mycurriculum/variables_and_data_types/advancedvariablemanipulation.js
@@ -3,3 +3,11 @@
 let varOne = '1';
 let closure;
 
+function logVariable (){
+    closure = varOne;
+    console.log(`This number is ${closure}`)
+}
+
+logVariable();
+
+// Use the forEach method to iterate over an array and perform an action.
